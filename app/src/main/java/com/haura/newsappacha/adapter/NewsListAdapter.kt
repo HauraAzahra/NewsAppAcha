ee0304b8792f6d1341a4e474eb25e080aa56929a
HP <haur.salsabila17@gmail.com>
2021-11-24 07:23:27 +0700

    Initial commit

---
+++ app/src/main/java/com/haura/newsappacha/adapter/NewsListAdapter.kt
@@ -0,0 +1,44 @@
+package com.haura.newsappacha.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.haura.newsappacha.R
+import com.haura.newsappacha.model.NewsModel
+
+class NewsListAdapter (private val listNews:ArrayList<NewsModel>) :
+        RecyclerView.Adapter<NewsListAdapter.ListViewHolder>(){
+
+    inner class ListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        var imgPhoto : ImageView = itemView.findViewById(R.id.iv_item_photo)
+        var textTitle : TextView = itemView.findViewById(R.id.tv_title)
+        var textDesc : TextView = itemView.findViewById(R.id.tv_deskripsi)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ListViewHolder {
+        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_news, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsListAdapter.ListViewHolder, position: Int) {
+        val news = listNews[position]
+        holder.textTitle.text = news.title
+        holder.textDesc.text = news.desc
+
+        Glide.with(holder.itemView.context)
+            .load(news.image)
+            .into(holder.imgPhoto)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listNews.size
+    }
+
+}
---
+++ app/src/main/java/com/haura/newsappacha/model/NewsModel.kt
@@ -0,0 +1,8 @@
+package com.haura.newsappacha.model
+
+data class NewsModel (
+    var image : Int = 0,
+    var title : String = "",
+    var desc : String = ""
+)
+
